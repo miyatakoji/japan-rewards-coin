6a5e0edcf7c12abbb86bf10f0c2ecb63722ffe03
miyatakoji <calcio.cogi@gmail.com>
2024-05-26 22:57:24 +0900

    feat: temp cash back logic

--- packages/hardhat/contracts/JapanRewardsCon.sol
+++ packages/hardhat/contracts/JapanRewardsCon.sol
@@ -5,15 +5,30 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RewardYen is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("RewardYen", "RYEN") {
+    uint256 public cashbackPercentage;
+    mapping(address => uint256) public points;
+
+    constructor(uint256 initialSupply, uint256 _cashbackPercentage) ERC20("RewardYen", "RYEN") {
         _mint(msg.sender, initialSupply);
+        cashbackPercentage = _cashbackPercentage;
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function setCashbackPercentage(uint256 _cashbackPercentage) public onlyOwner {
+        cashbackPercentage = _cashbackPercentage;
     }
 
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        bool success = super.transfer(recipient, amount);
+        if (success) {
+            uint256 cashback = (amount * cashbackPercentage) / 100;
+            points[recipient] += cashback;
+        }
+        return success;
+    }
+
+    function redeemPoints(address account, uint256 amount) public onlyOwner {
+        require(points[account] >= amount, "Not enough points");
+        points[account] -= amount;
+        _mint(account, amount);
     }
 }
