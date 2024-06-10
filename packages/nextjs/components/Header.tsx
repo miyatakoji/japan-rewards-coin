ecf7dfac0a18b7cb9399bfa1d49ebdfe695a1a71
miyatakoji <calcio.cogi@gmail.com>
2024-06-11 00:07:11 +0900

    change home template

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -15,15 +15,15 @@ type HeaderMenuLink = {
 };
 
 export const menuLinks: HeaderMenuLink[] = [
-  {
-    label: "Home",
-    href: "/",
-  },
-  {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
-  },
+  // {
+  //   label: "Home",
+  //   href: "/",
+  // },
+  // {
+  //   label: "Debug Contracts",
+  //   href: "/debug",
+  //   icon: <BugAntIcon className="h-4 w-4" />,
+  // },
 ];
 
 export const HeaderMenuLinks = () => {
